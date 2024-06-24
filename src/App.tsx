285b02fbc0cbe6a57cbe8d8ca0a5f62f05b32f15
markinascimento <markinhonascimento50@gmail.com>
2024-06-24 19:05:33 -0300

    feat: dwklqjhd

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Alter title</h1>
     </>
   );
 }
