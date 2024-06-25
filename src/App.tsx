f679c7bd59d0302a977f4fe917bdcbe300f42b96
markinascimento <markinhonascimento50@gmail.com>
2024-06-25 14:28:07 -0300

    feat: change title in app file

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Alter title</h1>
+      <h1> Pipeline CI/CD </h1>
     </>
   );
 }
