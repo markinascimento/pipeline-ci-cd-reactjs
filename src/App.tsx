f5a0cf83654a0b978e1b74d9064eebd394216746
markinascimento <markinhonascimento50@gmail.com>
2024-06-25 15:21:07 -0300

    feat: new commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1> Pipeline CI/CD </h1>
+      <h1> Pipeline CI/CD in reactjs </h1>
     </>
   );
 }
